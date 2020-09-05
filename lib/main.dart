c39f2a019f3286b3d0f1bf400fc186764afa1f9e
parentsmyworld <madhu@skylinkmsc.com>
2020-09-06 01:01:03 +0300

    pageschanged

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_detailsincard/First.dart';
+import 'package:flutter_detailsincard/pages/first.dart';
 
 void main(){
   runApp(MyApp());
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
           ),
           title: Center(child: Text("Students App")),
         ),
-        body: First(),
+        body: Firzt(),
       ),
     );
   }
